92a4ec74232dabd6dfa0687f21d265c4b576e386
VictorS8 <jjoaovvictorneves@gmail.com>
2021-04-14 02:42:05 -0300

    Indentation IDE
    - File with IDE Indentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:paycheck/pages/base.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:paycheck/pages/base.dart';
 
 void main() async {
   await GetStorage.init();
