c9ae20923cd8c0796350109aa074cd2f5be1be8f
VictorS8 <jjoaovvictorneves@gmail.com>
2021-04-14 00:20:52 -0300

    Adding GetStorage support
    - Just the beginning action with GetStorage
    - Update on get version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:paycheck/pages/base.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() => runApp(Base());
+void main() async {
+  await GetStorage.init();
+  runApp(Base());
+}
